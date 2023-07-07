e1c1c636381d07b36093847c4802a4f080298ebb
jihyojeon <jihyo@gm.gist.ac.kr>
2023-07-07 14:42:58 +0100

    feat: add types

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-module.exports = {
-  add: (a, b) => a + b,
-  subtract: (a, b) => a - b,
-  multiply: (a, b) => a * b,
-};
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
